71755d657be52e8924d8e92de7b25a8c51a24b29
Brendan Tierney <tiernebre@gmail.com>
2021-12-12 21:35:17 -0700

    add more details

--- src/SomeVueComponent.tsx
+++ src/SomeVueComponent.tsx
@@ -2,19 +2,33 @@ import { ref, defineComponent } from 'vue-demi'
 
 type SomeVueComponentProps = {
   /**
-   * WHAT.THE.FUCK
+   * Message that is displayed in the Vue component.
    */
-  wtf: string;
+  message: string;
 }
 
 /**
  * COOL BEANS
  */
 const SomeVueComponent = defineComponent<SomeVueComponentProps>({
-  setup() {
-    const state = ref(1)
+  setup(props, attrs) {
+    const numberOfClicks = ref(0)
 
-    return () => <div>hi {state.value.toString()}</div>
+    const onClick = () => {
+      numberOfClicks.value++
+    }
+
+    const emitCoolEvent = () => {
+      attrs.emit('cool')
+    }
+
+    return () => <div>
+      <h1>Welcome to Vue 3 Design System Demonstration</h1>
+      <p>Prop `message` = {props.message}</p>
+      <p>State `numberOfClicks` = {numberOfClicks.value}</p>
+      <button onClick={onClick}>Click!</button>
+      <button onClick={emitCoolEvent}>COOL!</button>
+    </div>
   }
 })
 
