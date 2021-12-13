2d3707c2c088ac79ba3683a694aae23f516116d4
Brendan Tierney <tiernebre@gmail.com>
2021-12-12 19:22:59 -0700

    updated docs

--- src/SomeVueComponent.tsx
+++ src/SomeVueComponent.tsx
@@ -7,6 +7,9 @@ type Props = {
   wtf: string;
 } & ButtonHTMLAttributes;
 
+/**
+ * Some cool Vue component in JSX. Neat-o JSX IS AWESEOMEEE
+ */
 const SomeVueComponent = defineComponent<Props>({
   render () {
     return <div>HI: {this.wtf}</div>
