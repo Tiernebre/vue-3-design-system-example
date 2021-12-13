cbcb10ca57ef867b77071efe555cc589e995148b
Brendan Tierney <tiernebre@gmail.com>
2021-12-12 19:24:13 -0700

    revert back

--- src/SomeVueComponent.tsx
+++ src/SomeVueComponent.tsx
@@ -1,15 +1,12 @@
-import { ButtonHTMLAttributes, defineComponent } from 'vue-demi'
+import { defineComponent } from 'vue-demi'
 
 type Props = {
   /**
    * WHAT.THE.FUCK
    */
   wtf: string;
-} & ButtonHTMLAttributes;
+}
 
-/**
- * Some cool Vue component in JSX. Neat-o JSX IS AWESEOMEEE
- */
 const SomeVueComponent = defineComponent<Props>({
   render () {
     return <div>HI: {this.wtf}</div>
