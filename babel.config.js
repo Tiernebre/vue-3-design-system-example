fa8342dad33146a57acdd572b75e7e8f78ca8697
Brendan Tierney <tiernebre@gmail.com>
2021-12-12 18:40:09 -0700

    is vue 2 plugin

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,13 @@ const { isVue2, isVue3 } = require('vue-demi')
 module.exports = {
   presets: [
     "@babel/env",
-    "@babel/typescript"
+    "@babel/typescript",
+    isVue2 && [
+      '@vue/babel-preset-jsx',
+      {
+        compositionAPI: true,
+      },
+    ],
   ],
   plugins: [
     isVue3 && "@vue/babel-plugin-jsx"
