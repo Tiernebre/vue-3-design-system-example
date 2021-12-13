548958a01792bc5aff5a9df4aa865109db3d5400
Brendan Tierney <tiernebre@gmail.com>
2021-12-12 21:10:02 -0700

    meta support

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,9 @@ module.exports = {
   presets: [
     "@babel/env",
     "@babel/typescript",
-    isVue2 && '@vue/babel-preset-jsx',
+    isVue2 && ['@vue/babel-preset-jsx', {
+      compositionAPI: true
+    }],
   ].filter(Boolean),
   plugins: [
     isVue3 && "@vue/babel-plugin-jsx"
