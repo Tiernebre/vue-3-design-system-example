29e5b510d65db9b37ecc81858c511f6a5dc7e4b5
Brendan Tierney <tiernebre@gmail.com>
2021-12-12 18:20:44 -0700

    uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
+import { babel } from '@rollup/plugin-babel';
 import pkg from './package.json';
-import uglify from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
 
 const extensions = [
   '.ts', '.tsx'
