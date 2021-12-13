5cb6bbbdd22c2b21d1ddc86a57c545ea3391c64a
Brendan Tierney <tiernebre@gmail.com>
2021-12-12 17:20:25 -0700

    babel setup

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,5 @@ module.exports = {
   "presets": [
     "@babel/env",
     "@babel/typescript"
-  ],
-  "plugins": [
-    "@babel/proposal-class-properties",
-    "@babel/proposal-object-rest-spread"
   ]
 }
