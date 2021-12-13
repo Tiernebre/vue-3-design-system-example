d60ffe53fc600f45e55901c8821d98499a104665
Brendan Tierney <tiernebre@gmail.com>
2021-12-12 18:19:45 -0700

    uglify

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import pkg from './package.json';
+import uglify from 'rollup-plugin-uglify'
 
 const extensions = [
   '.ts', '.tsx'
@@ -25,6 +26,8 @@ const config = {
       babelHelpers: 'inline',
       include: ['src/**/*'],
     }),
+
+    uglify()
   ],
 
   output: [
