a166d6dedf547d916bcc9244e461d15ca8d2a4eb
Brendan Tierney <tiernebre@gmail.com>
2021-12-12 19:18:19 -0700

    fix typos

--- build/postinstall.js
+++ build/postinstall.js
@@ -9,12 +9,12 @@ const isVue2 = +version.split('.')[0] === 2
 
 if (isVue2) {
   console.log('[Vue 3 Design System Example] Switch main field for Vue 2')
-  pkg.main = 'dist/vue-2/vue-ds.umd.js'
-  pkg.module = 'dist/vue-2/vue-ds.es.js'
+  pkg.main = './dist/vue-2/vue-ds.umd.js'
+  pkg.module = './dist/vue-2/vue-ds.es.js'
 } else {
   console.log('[Vue 3 Design System Example] Switch main field for Vue 3')
-  pkg.main = 'dist/vue/vue-ds.umd.js'
-  pkg.module = 'dist/vue/vue-ds.es.js'
+  pkg.main = './dist/vue/vue-ds.umd.js'
+  pkg.module = './dist/vue/vue-ds.es.js'
 }
 
 fs.writeFileSync(
