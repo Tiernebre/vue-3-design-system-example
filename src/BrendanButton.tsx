f1975f49c32060b73133c645c21ab42658e76b06
Brendan Tierney <tiernebre@gmail.com>
2021-12-13 18:36:00 -0700

    build

--- src/BrendanButton.tsx
+++ src/BrendanButton.tsx
@@ -1,5 +1,6 @@
 import { ButtonHTMLAttributes } from 'vue'
 import { defineComponent } from 'vue-demi'
+import { handleAttrs } from './handleVue2Props'
 
 type BrendanButtonProps = ButtonHTMLAttributes
 
@@ -7,6 +8,10 @@ export const BrendanButton = defineComponent<BrendanButtonProps>({
   setup(props, ctx) {
     console.log('received props = ', props)
     console.log('received ctx = ', ctx)
-    return () => <button {...props}>{ctx.slots && ctx.slots.default && ctx.slots.default()}</button>
+    const mergedProps = handleAttrs({
+      ...props,
+      ...ctx
+    })
+    return () => <button {...mergedProps}>{ctx.slots && ctx.slots.default && ctx.slots.default()}</button>
   }
 })
